9a573afa747d16b3fd604c8d65e1b21fe7fdf72f
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-31 13:04:07 -0400

    added routing/components stuff to app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,43 @@
 
 const app = angular.module('MealApp', ['ui.router']);
 
+app.config(function ($stateProvider){
 
+    $stateProvider.state({
+        name: 'start',
+        url: '/start',
+        component: 'startPage',
+    });
+    $stateProvider.state({
+        name: 'home',
+        url: '/home',
+        component: 'homePage',
+    });
+    $stateProvider.state({
+        name: 'cook',
+        url: '/cook',
+        component: 'cookPage',
+    });
+    $stateProvider.state({
+        name: 'cookconfirm',
+        url: '/cook-confirm',
+        component: 'cookconfirmPage',
+    });
+    $stateProvider.state({
+        name: 'meallist',
+        url: '/meal-list',
+        component: 'meallistPage',
+    });
+    $stateProvider.state({
+        name: 'mealdetail',
+        url: '/meal/:mealID',
+        component: 'mealdetailPage',
+    });
+    $stateProvider.state({
+        name: 'eatconfirm',
+        url: '/eat-confirm',
+        component: 'eatconfirmPage',
+    });
+
+});
 
