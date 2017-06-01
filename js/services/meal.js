979597ff8254a242cf6a5f7bf6bf203b333d8081
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-06-01 15:21:36 -0400

    get all meals endpoint in api is working

--- js/services/meal.js
+++ js/services/meal.js
@@ -18,9 +18,13 @@ module.exports = {
                     // find out what the length of response is (how many meals?)
                     // loop through and push object into availableMeals array
                     console.log(response.data);
+                    for (let i = 0; i < response.length; i++) {
+                        availableMeals.push(response.data); // I feel like this should work but it doesn't seem to, might
+                                                            // be something in the template/angular stuff. I'll talk to Neilson.
+                    };
 
                 });
-            }
+            },
         }
     }
 }
