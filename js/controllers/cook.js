5a87156c0110006ae0e4d66e5e736701578cdbe8
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-06-01 10:18:46 -0400

    Angularized meal service and controller.  Added package.json

--- js/controllers/cook.js
+++ js/controllers/cook.js
@@ -3,8 +3,17 @@ module.exports = {
     func: function ($scope, $stateParams, MealService) {
         
         $scope.add = function(meal){
-            MealService.postMeal();
-            console.log('meal added');
+
+            const newMeal = {
+                name: $scope.meal_name,
+                recipe: $scope.recipe_url,
+                servings: parseInt($scope.servings, 10),
+                available: $scope.pickup_time,
+                cuisine: $scope.category,
+                add_info: $scope.add_info,
+            };
+
+            MealService.postMeal(newMeal);
         }
     },
 };
