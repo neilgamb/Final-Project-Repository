c0a5d534d7bb00622faa4bceb0e11c369536d112
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-06-08 10:53:12 -0400

    finally got the merge right

--- js/controllers/meal.js
+++ js/controllers/meal.js
@@ -37,34 +37,35 @@ module.exports = {
           MealService.postOrder(newOrder);
 
         };
-      },
-
-        function initMap(addressMap) {
-
-          var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            center: { lat: -34.397, lng: 150.644 }
-          });
-
-          var geocoder = new google.maps.Geocoder();
-
-          geocodeAddress(geocoder, map, addressMap);
-        }
-
-      function geocodeAddress(geocoder, resultsMap, addressMap) {
-        var address = addressMap;
-        geocoder.geocode({ 'address': address }, function (results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
       };
     });
   }
 }
+
+
+function initMap(addressMap) {
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: { lat: -34.397, lng: 150.644 }
+  });
+
+  var geocoder = new google.maps.Geocoder();
+
+  geocodeAddress(geocoder, map, addressMap);
+}
+
+function geocodeAddress(geocoder, resultsMap, addressMap) {
+  var address = addressMap;
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+};
