f63ac8f047dcce48ff55a096906b012d253b29f7
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-06-12 14:50:51 -0400

    logout component mostly working right now, but still getting a 404. something re-directing, almost there

--- js/controllers/logout.js
+++ js/controllers/logout.js
@@ -3,7 +3,7 @@ module.exports = {
     func: function ($scope, $state, $stateParams, UserService) {
         $scope.logout = function () {
 
-            // basically this function takes the "enabled" property and sets it false (I think?)
+            
             UserService.attemptLogout();
             $state.go('start');
         };
