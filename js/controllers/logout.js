2c41861502b195af1353d27a676dc041f26ec832
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-06-12 15:35:43 -0400

    hopefully get logout working soon

--- js/controllers/logout.js
+++ js/controllers/logout.js
@@ -3,7 +3,6 @@ module.exports = {
     func: function ($scope, $state, $stateParams, UserService) {
         $scope.logout = function () {
 
-            
             UserService.attemptLogout();
             $state.go('start');
         };
