133e766d821562ce4730cfb35e1c5546559dbe0e
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-06-13 14:50:08 -0400

    commit 2:50 tuesday

--- js/services/user.js
+++ js/services/user.js
@@ -26,9 +26,16 @@ module.exports = {
                 return $http.get('https://thawing-waters-96173.herokuapp.com/logout');
             },
 
-            numberOfTokens() {
-
+            numberOfTokens(userID) {
+                let tokens = 0;
                 // write a function that returns the number of tokens
+                $http.get('https://thawing-waters-96173.herokuapp.com/select-user/' + userID).then(function(response) {
+                    
+                    // in here put the function that returns tokens = user.token; 
+                });
+                // Steps: I think this is the right GET request, but if it's not, make sure you are
+                // GET-ing the right user object, return the number of tokens as a function of that response;
+                
                 
             },
         };
