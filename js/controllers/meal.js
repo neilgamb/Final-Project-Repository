8c617acdfa999706c060c51b3ebd17522b99243a
Guest <guest@Guests-MacBook-Air.local>
2017-06-15 16:10:45 -0400

    Final styling

--- js/controllers/meal.js
+++ js/controllers/meal.js
@@ -36,7 +36,7 @@ module.exports = {
 function initMap(addressMap) {
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 12,
     center: { lat: -34.397, lng: 150.644 }
   });
 
